8cadb44b4f42758af599723686a43862d426fed9
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-01-17 08:18:03 +0000

    Improved distance calculation to the BWQ data

--- process.js
+++ process.js
@@ -1,73 +1,101 @@
+//Processes the data files to produce the static JSON data the musical front end uses
 var fs=require("fs");
-var oData = JSON.parse(fs.readFileSync("./sourcedata/uk_lighthouses.geojson").toString());
+
 var LatLon    = require('geodesy').LatLonEllipsoidal;
 var OsGridRef = require('geodesy').OsGridRef;
 var Dms       = require('geodesy').Dms;
 
-
 var basicCSV = require("basic-csv");
+//basicCSV is not great but because the data we had coming it was labelled badly it seemed the best fit
 
-basicCSV.readCSV("./sourcedata/cwtb04ab_uksite_201411.csv", {dropHeader: true}, function (error, rows) {
-	  //console.log(rows); // displays a nested array of arrays
+//Data sources 
+
+//Light house data a GEO JSON file from OpenStreetMap using overpass turbo
+// http://overpass-turbo.eu/s/dMO 
+/* Query:
+[out:json][timeout:25];
+{{geocodeArea:UK}}->.searchArea;
+(
+  node["man_made"="lighthouse"](area.searchArea);
+);
+out body;
+>;
+out skel qt;
+*/ 
+var sLighthouseFile = "./sourcedata/uk_lighthouses.geojson";
+
+//Bathing water quality data
+//We would have been better using the API directly but currenty have a csv
+// TODO : #2 on github
+var sBWQ_File = "./sourcedata/cwtb04ab_uksite_201411.csv"
+
+var oIncomingLighthouses = JSON.parse(fs.readFileSync(sLighthouseFile).toString());
+
+
+basicCSV.readCSV(sBWQ_File, {dropHeader: true}, function (error, rows) {
 	  
+	  // Calc the LAT /Lon for each sampling site (the API data has this)
 	  var aSites = rows.map(function(aRow){
-		 //console.log(aRow);
 		 try {
-			 var grid = OsGridRef.parse(aRow[2])
-			 var latlong = OsGridRef.osGridToLatLon(grid); // p.toString(): 52°39′29″N, 001°42′58″E
+			 var grid = OsGridRef.parse(aRow[2]);
+			 var latlong = OsGridRef.osGridToLatLon(grid);
 			 
 			 //console.log(latlong);
 			 var oSite = {
 				 "site_name":aRow[1],
+				 "dist":null,
 				 "lat":latlong.lat,
 				 "lon":latlong.lon,
 				 "e-coli": aRow[8],
 				 "enterocci":aRow[12]
-			 }
+			 };
 			 return oSite;
 		 } catch (e) {
 			 console.log(aRow[1], "Bathing site OS Grid Ref "+aRow[2]+" failed to parse.");
 		 }
 	  });
 	  
+	  //Drop sites we couldn't find at lon for
 	  aSites = aSites.filter(function(rSite){
-		  
 		 return (typeof rSite  !== "undefined") && (typeof rSite.lat !== "undefined"); 
-		  
 	  });
 
-	//console.log(aSites);
-	
+	//Dump the modified BWQ data in case we need it in the UI
 	fs.writeFileSync("./data/cwtb04ab_uksite_201411.json", JSON.stringify(aSites, null, 4));
 	
+	//Create a parallel list of lat lon for sites to ease calculation later 
+	var aSitesWithPoint = aSites.map(function(oSite){
+		return  new LatLon(oSite.lat, oSite.lon);//, "OSGB36");
+		//return oSite;
+	});
 	
-	var aWithNearest = oData.features.map(function(oLighthouse){
+	//Add the neardt BWQ sample to each lighthouse
+	var aWithNearest = oIncomingLighthouses.features.map(function(oLighthouse){
 		var oLPos =  new LatLon(oLighthouse.geometry.coordinates[1], oLighthouse.geometry.coordinates[0]);
-		var aDists = aSites.map(function(oSite){
-			var oSPos =  new LatLon(oSite.lat, oSite.lon);
 		
-			oSite.dist = Math.round(oLPos.distanceTo(oSPos) / 1000);
+		//Calc the distance for each BWQ site to this lighthouse
+		var aDists = aSites.map(function(oSite, iKey){
 			
+			oSite.dist = Math.round(aSitesWithPoint[iKey].distanceTo(oLPos)/1000); //km
+			//console.log("oLPos",oLPos.lat, oLPos.lon, "aSitesWithPoint[iKey]", aSitesWithPoint[iKey].lat, aSitesWithPoint[iKey].lon, "oSite.dist", oSite.dist);
 			return oSite;
 		});
 		
+		//sort the BWQ site by distance to this lighthouse
 		aDists.sort(function(a,b){
-			if(a.dist < b.dist) {
-				return -1;
-			} else if(a.dist > b.dist) {
-				return 1;
-			}
-			return 0;
+			return a.dist - b.dist;
 		});
-			
-		oLighthouse.bathing_data = aDists[0];
 		
+		//Attach the mearset (first in the list) to this lighthouse
+		oLighthouse.bathing_data = aDists[0];
 		return oLighthouse;
 	});
 	
-	oData.features = aWithNearest;
+	//Reattach the modified lighthouses to the geojson
+	oIncomingLighthouses.features = aWithNearest;
 	
-	fs.writeFileSync("./data/lighthouses.geojson", JSON.stringify(oData, null, 4));
+	//Write to disk
+	fs.writeFileSync("./data/lighthouses.geojson", JSON.stringify(oIncomingLighthouses, null, 4));
 	
 });
 
